1df3f931dfbac0fb6dbf18b13c896b9e30ba68ba
Abigail Gray <argray@umass.edu>
2020-09-06 22:49:03 -0400

    added old files

---
+++ lab1_abigailgray.R
@@ -0,0 +1,22 @@
+###Exercise 1
+x <- 3
+y <- 15
+x + y
+y - x
+x * y
+y / x
+
+###Exercise 2
+x <- 2
+y <- 3
+x^5 > y^4
+
+###Exercise 3
+x <- c(22,62,148,43,129)
+y <- sum(x)
+y
+x / y
+
+#Exercise 4
+nucleotides <- c("A","T","C","G")
+sort(nucleotides)
